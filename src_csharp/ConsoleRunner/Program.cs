8ee8721b6912e0e11d0195daaf83977dc31772b2
Paul Herrera <paul.herrera@dimensional.com>
2021-03-28 08:45:47 -0500

    Added first passing test

--- src_csharp/ConsoleRunner/Program.cs
+++ src_csharp/ConsoleRunner/Program.cs
@@ -1,12 +1,56 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using CommandLine;
+using Core.TheBlocksProblem;
 
 namespace ConsoleRunner
 {
-    class Program
+    public class Program
     {
+        public class Options
+        {
+            [Option('p', "problem", Required = true, HelpText = "The name of the problem to run.")]
+            public string Problem { get; set; }
+
+            [Option('o', "output", Required = true, HelpText = "The destination directory for the output files.")]
+            public string OutputDirectory { get; set; }
+
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Parser.Default.ParseArguments<Options>(args)
+                .WithParsed<Options>(o =>
+                {
+                    if (o.Problem == "TheBlocksProblem")
+                    {
+                        var theBlocksProblemSolution = new TheBlocksProblemSolution();
+                        
+                        var lines = GetTestFileLines();
+
+                        var enumerable = theBlocksProblemSolution.GetSolution(lines);
+
+                        File.WriteAllLines("c:\\temp\\output000.txt", enumerable);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Nothing to run.");
+                    }
+                });
         }
+
+        public static IEnumerable<string> GetTestFileLines()
+        {
+            var file = new System.IO.StreamReader(@"TheBlocksProblem\\input000.txt");
+
+            string line;
+
+            while ((line = file.ReadLine()) != null)
+            {
+                yield return line;
+            }
+        }
+
     }
 }
---
+++ src_csharp/Core/TheBlocksProblem/TheBlocksProblemSolution.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+
+namespace Core.TheBlocksProblem
+{
+    public class TheBlocksProblemSolution
+    {
+        public IEnumerable<string> GetSolution(
+            IEnumerable<string> testFileLines)
+        {
+            foreach (var testFileLine in testFileLines)
+            {
+                yield return "";
+            }
+        }
+    }
+}
